3a4e78c3f4838650715570c3e3b2572b03806caa
angonzalezs <angonzalezs@LP_ANGONZALEZS>
2023-08-02 16:10:21 -0600

    primer commit

---
+++ src/main/java/FundemantosAngel/FundamentosAngelApplication.java
@@ -0,0 +1,13 @@
+package FundemantosAngel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosAngelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosAngelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FundemantosAngel/FundamentosAngelApplicationTests.java
@@ -0,0 +1,13 @@
+package FundemantosAngel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosAngelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
